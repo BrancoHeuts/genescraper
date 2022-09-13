182ac0a21d858ef4cfa270a45bb7b0863d6c5581
Branco-Heuts <brancoheuts@gmail.com>
2022-09-13 21:02:36 +0200

    Bootstrap

--- scrape_data.py
+++ scrape_data.py
@@ -1,77 +1,91 @@
 from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+from tqdm import tqdm
 
 # TODO: 1. catch errors when scraping and replace output with NA
-# TODO: 2. Loading bar?
 # TODO: 3. Make index.html more readable
 # TODO 4: Fix nav bar
 # TODO 5: Implement search bar?
-# TODO 6: Make gene list unique
 
 url = "https://www.genecards.org/cgi-bin/carddisp.pl?gene="
 
 
-gene_list = pd.read_csv("./gene_list.csv", header=None).iloc[0]
+gene_list = pd.read_csv("./gene_list.csv", header=None).iloc[0].unique()
 
 data = []
-for gene in gene_list:
+for gene in tqdm(gene_list):
 
-    req = Request(
-        url=url + gene,
-        headers={'User-Agent': 'Mozilla/5.0'}
-    )
-    webpage = urlopen(req).read()
+    try:
+        req = Request(
+            url=url + gene,
+            headers={'User-Agent': 'Mozilla/5.0'}
+        )
+        webpage = urlopen(req).read()
 
-    # Load html code from a url
-    soup = bs(webpage, "html.parser")
+        # Load html code from a url
+        soup = bs(webpage, "html.parser")
 
-    # ----- Aliases -----
-    # Main name
-    main_name = soup.body.find("span", class_='aliasMainName').get_text()
+        # ----- Aliases -----
+        # Main name
+        main_name = soup.body.find("span", class_='aliasMainName').get_text()
 
-    # Description
-    descriptions = soup.body.find_all(itemprop='description')
-    description_list_stripped = [item.get_text() for item in descriptions]
+        # Description
+        descriptions = soup.body.find_all(itemprop='description')
+        description_list_stripped = [item.get_text() for item in descriptions]
 
-    # Alternate names
-    alternate_names = soup.body.find_all(itemprop='alternateName')
-    alternate_name_list_stripped = [item.get_text() for item in alternate_names]
+        # Alternate names
+        alternate_names = soup.body.find_all(itemprop='alternateName')
+        alternate_name_list_stripped = [item.get_text() for item in alternate_names]
 
-    # ----- Summaries -----
-    summaries = soup.body.find_all("div", class_="gc-subsection-header")
+        # ----- Summaries -----
+        summaries = soup.body.find_all("div", class_="gc-subsection-header")
 
-    for i in summaries:
-        stripped_string = i.get_text()
+        for i in summaries:
+            stripped_string = i.get_text()
 
-        # Entrez Gene Summary
-        if f"Entrez Gene Summary for {gene} Gene" in stripped_string:
-            entrez_summary = i.find_next("p").get_text()
+            # Entrez Gene Summary
+            if f"Entrez Gene Summary for {gene} Gene" in stripped_string:
+                entrez_summary = i.find_next("p").get_text()
 
-        # UniProtKB/Swiss-Prot Summary
-        if f"UniProtKB/Swiss-Prot Summary for {gene} Gene" in stripped_string:
-            uniprot_summary = i.find_next("p").get_text()
-            uniprot_summary = uniprot_summary.split("\r\n")[1]
+            # UniProtKB/Swiss-Prot Summary
+            if f"UniProtKB/Swiss-Prot Summary for {gene} Gene" in stripped_string:
+                uniprot_summary = i.find_next("p").get_text()
+                uniprot_summary = uniprot_summary.split("\r\n")[1]
 
-    # ----- Molecular function -----
-    function = soup.body.find_all("div", class_="gc-subsection-inner-wrap")
+        # ----- Molecular function -----
+        function = soup.body.find_all("div", class_="gc-subsection-inner-wrap")
 
-    for i in function:
-        stripped_string = i.get_text()
-        if "Function:" in stripped_string:
-            function_string = i.find_next("li").get_text()
-            function_list = function_string.split(".")
+        for i in function:
+            stripped_string = i.get_text()
+            if "Function:" in stripped_string:
+                function_string = i.find_next("li").get_text()
+                function_list = function_string.split(".")
 
-            # Remove redundant strings and list items
-            function_list[0] = function_list[0].replace("\r\n", "")
-            function_list = function_list[:-1]
+                # Remove redundant strings and list items
+                function_list[0] = function_list[0].replace("\r\n", "")
+                function_list = function_list[:-1]
 
-    data_item = {
-        'gene_name': main_name,
-        'alias_description': description_list_stripped,
-        'alias_alternate_name': alternate_name_list_stripped,
-        'entrez_summary': entrez_summary,
-        'uniport_summary': uniprot_summary,
-        'molecular_function': function_list
-    }
-    data.append(data_item)
+        data_item = {
+            'gene_name': main_name,
+            'alias_description': description_list_stripped,
+            'alias_alternate_name': alternate_name_list_stripped,
+            'entrez_summary': entrez_summary,
+            'uniport_summary': uniprot_summary,
+            'molecular_function': function_list
+        }
+        data.append(data_item)
+
+    except HTTPError as err:  # If gene does not exist
+        if err.code == 404:
+            data_item = {
+                'gene_name': "NA",
+                'alias_description': ["NA"],
+                'alias_alternate_name': ["NA"],
+                'entrez_summary': "NA",
+                'uniport_summary': "NA",
+                'molecular_function': ["NA"]
+            }
+        else:
+            raise
